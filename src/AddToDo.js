20bf626c43ae904e076cceec556738c7ca7284e9
ASUS <Yehor Kholodiuk>
2022-09-24 22:08:09 +0300

    initial commit

--- src/AddToDo.js
+++ src/AddToDo.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react';
 
 const AddToDo = (props) => {
     const [inputTask, setInputTask] = useState('')
+    function saveButtonHandler (){
+        props.addNewTask(inputTask)
+        props.toggleAddTask()
+    }
     return (
         <div>
             {inputTask}
             <input type="text" placeholder="type here" value={inputTask}
             onChange={(event) => setInputTask(event.target.value)}
             />
-            <button onClick={() => props.addNewTask(inputTask)}>Save</button>
+            <button onClick={saveButtonHandler}>Save</button>
             <button onClick={props.toggleAddTask}>Cancel</button>
         </div>
     );
